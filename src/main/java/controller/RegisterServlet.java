2d56065085fef44d5714ac09c01253188e18e05a
Harsha <harsha9353140091@gmail.com>
2026-05-05 10:52:15 +0530

    add doGet to RegisterServlet

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -1,6 +1,4 @@
 package controller;
-
-
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -9,17 +7,19 @@ import model.User;
 
 public class RegisterServlet extends HttpServlet {
 
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/register.jsp").forward(req, res);
+    }
+
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
         User user = new User();
         user.setName(req.getParameter("name"));
         user.setEmail(req.getParameter("email"));
         user.setPassword(req.getParameter("password"));
         user.setRole("student");
-
         UserDAO dao = new UserDAO();
-
         if (dao.register(user)) {
             res.sendRedirect("login.jsp");
         } else {
